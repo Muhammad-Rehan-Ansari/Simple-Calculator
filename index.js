56f0d9ee214400f0a42af3d144aeae8bd7da1a06
Muhammad-Rehan-Ansari <Muhammadrehanansari07@gmail.com>
2024-03-22 01:47:18 +0500

    push my calculator to github

---
+++ index.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter Your First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Your Second Number", type: "number", name: "secondNumber" },
+    { message: "Please Select One Of The Operator To Perfom Action", type: "list", name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division", "Modulus", "Exponentiation"], },
+]);
+//Here We Use Conditional Statement (If, else if & else)
+if (answer.operator === "Addition") {
+    console.log("Your Output Result is =", answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log("Your Output Result is =", answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log("Your Output Result is =", answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log("Your Output Result is =", answer.firstNumber / answer.secondNumber);
+}
+else if (answer.operator === "Modulus") {
+    console.log("Your Output Result is =", answer.firstNumber % answer.secondNumber);
+}
+else if (answer.operator === "Exponentiation") {
+    console.log("Your Output Result is =", answer.firstNumber ** answer.secondNumber);
+}
+else {
+    console.log("Something Is Wrong, Please Select Valid Operators OR Valid Input Number");
+}
